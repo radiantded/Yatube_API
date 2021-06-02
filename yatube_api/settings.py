f3a93ea418724d7abb77987deaf76391f2015705
Enver <radiantded@gmail.com>
2021-06-02 21:54:32 +0300

    ok

--- yatube_api/settings.py
+++ yatube_api/settings.py
@@ -93,7 +93,7 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
     'DEFAULT_FILTER_BACKENDS': [
